dd8e981b786c08773eb3ec7d87f4f81abf9d3efe
AlexanderPilz <51150328+xcomagent95@users.noreply.github.com>
2021-08-18 13:17:52 +0200

    basic server
    setup basic server functions and websites

---
+++ public/routes/delete.js
@@ -0,0 +1,52 @@
+var express = require('express');
+const app = express();
+var router = express.Router();
+const assert = require('assert');
+
+//Here we are configuring express to use body-parser as middle-ware
+app.use(express.json());
+app.use(express.urlencoded());
+
+//MongoConnect
+//-------------->>>>Hier muss die passende Datenbank und die passende Collection angegeben werden!!!!!<<<<--------------
+const url = 'mongodb://localhost:27017' // connection URL
+const dbName = 'tourguidedb' // database name
+const collectionName = 'locations' // collection name
+
+//----------------------------------------------------------------------------------------------------------------------
+const MongoClient = require('mongodb').MongoClient
+const client = new MongoClient(url) // mongodb client
+
+// Delete Router
+router.post('/removeLocation', function(req, res, next)
+{
+    client.connect(function(err)
+    {
+        const db = client.db(dbName)
+        const collection = db.collection(collectionName)
+        //check if number exists
+        collection.find({nummer: req.body.gNummer}).toArray(function(err, docs)
+        {
+            assert.strictEqual(err, null)
+        
+            if(docs.length >= 1){
+                console.log(req.body);
+
+                assert.strictEqual(null, err);
+                
+                console.log(req.body);
+                //delete Document
+                collection.deleteOne({nummer: req.body.gNummer}, function(err, results){
+                    assert.strictEqual(err, null)
+                })
+                res.sendFile(__dirname + "/done.html")
+            }
+            else {
+                res.sendFile(__dirname + "/error_nonexistent_number.html")
+            }
+            
+        })
+
+    })
+})
+module.exports = router; //export as router
---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express'); //require for express
+const app = express(); //create express app
+const port = 3000;
+
+//Parser for Request
+app.use(express.json());
+app.use(express.urlencoded());
+
+//Routers
+var searchRouter = require('./public/routes/search.js'); //require search router
+var addRouter = require('./public/routes/add.js'); //require add router
+var updateRouter = require('./public/routes/update.js'); //require update router
+var deleteRouter = require('./public/routes/delete.js'); //requiredelete router
+
+//Folders
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/node_modules'));
+
+//Usages (mainly routers)
+app.use('/search', searchRouter);
+app.use('/add', addRouter);
+app.use('/update', updateRouter);
+app.use('/delete', deleteRouter);
+
+//Gets
+app.get("/map", (req, res) => { res.sendFile(__dirname + "/public/map.html"); });
+app.get("/editor", (req, res) => { res.sendFile(__dirname + "/public/editor.html"); });
+app.get("/impressum", (req, res) => { res.sendFile(__dirname + "/public/impressum.html"); });
+
+//Listener
+app.listen(port, () => {
+    console.log(`>Server started`);
+	console.log(`>Example app listening at http://localhost:${port}`);
+    }
+);
