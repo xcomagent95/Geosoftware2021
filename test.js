55db7e7a88220b941ea86485ee063bd6652d5dd9
jbalzer12 <61976072+jbalzer12@users.noreply.github.com>
2021-09-18 20:12:43 +0200

    first tests written

--- test.js
+++ test.js
@@ -1,24 +1,14 @@
-/*const { expect } = require("chai");
+const { expect } = require("chai");
 let got = require("got");
 let port = 3000;
-*/
-/*
+
 describe ("Routing test", () =>
 {
-    let urlAdd = `http://localhost:${post}/add`;
+    let urlAdd = `http://localhost:${port}/map`;
 
     test("/add route: return status 200", async () =>
        {
            const response = await got(urlAdd);
            expect(response.statusCode).toBe(200);
        })
-})
-*/
-
-describe ("my first test", () => 
-{
-    test("should return 3", () =>
-    {
-        expect(1+2).toBe(3);
-    })
 })
